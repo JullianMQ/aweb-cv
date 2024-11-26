6ab08059c8b97fe01050cafccd80c64187795f7f
JullianMQ <jullianmq@gmail.com>
2024-11-26 21:24:23 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PersonalInfoComponent } from './personal-info/personal-info.component'
+import { EducationHistoryComponent } from './education-history/education-history.component'
+import { ProjectsComponent } from './projects/projects.component'
+import { ReferencesComponent } from './references/references.component'
+import { ToolsComponent } from './tools/tools.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    PersonalInfoComponent,
+    EducationHistoryComponent,
+    ProjectsComponent,
+    ReferencesComponent,
+    ToolsComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
