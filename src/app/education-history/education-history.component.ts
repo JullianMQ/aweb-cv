da2d46e4a7dcfc4aa96c2206205be3fc33240dde
JullianMQ <jullianmq@gmail.com>
2024-11-26 21:25:06 +0800

    finished the cv

---
+++ src/app/education-history/education-history.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-education-history',
+  imports: [],
+  templateUrl: './education-history.component.html',
+  styleUrl: './education-history.component.css'
+})
+export class EducationHistoryComponent {
+
+}
